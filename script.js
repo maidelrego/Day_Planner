bb5a6e358fee3fe34262938ac6adf90657ee717a
Legend <maesabroso@gmail.com>
2020-01-10 19:16:43 -0600

    first

---
+++ script.js
@@ -0,0 +1,37 @@
+var now = new Date();
+var events = $('textarea');
+function display_c(){
+    var refresh=1000; 
+    mytime=setTimeout('display_ct()',refresh);
+}
+ 
+function display_ct() {
+    var d = new Date();
+    document.getElementById('currentDay').innerHTML = d;
+    display_c();
+}
+
+
+
+for( var i = 0; i < $('textarea').lenght; i++){
+    
+    if(events.attr('aria-valuetext') < 12){
+        $('textarea').addClass("past")
+        $('textarea').prop("readonly", "true");
+    }
+    if(events.attr('aria-valuetext') > 12){
+        $('textarea').addClass("future");
+    }
+
+}
+console.log($('textarea').lenght);
+
+// if(now.getHours() == 13){
+//     $('#textarea13').addClass("present")
+// }
+
+// if(now.getHours() >! 14){
+//     $('#textarea14').addClass("future")  
+// }
+
+
