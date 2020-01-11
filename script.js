5e716864d2a8a9e53565ed6c39c72c1a085a5d2e
Legend <maesabroso@gmail.com>
2020-01-11 15:16:14 -0600

    almost done

--- script.js
+++ script.js
@@ -1,59 +1,66 @@
+// variable that is going to hold my current time and date 
 var currentDate = moment();
-document.getElementById('currentDay').innerHTML = currentDate;
+$('#currentDay').text(currentDate);
 
+// variable holding all my inputs
 var allInputs = $(":input:text");
 
-console.log(allInputs)
+// variable holding my current hour
+var curreentHour = moment().hour();
+// console.log(curreentHour);
 
-
+// a for loop to go trough all my inputs ids
 for(var i= 0; i < allInputs.length; i++){
+
+    // if staments comparing ids vs current hour
+
+    if($(allInputs[i]).prop('id') < curreentHour ){
+        $(allInputs[i]).addClass("past");
+        $(allInputs[i]).prop("readonly", "true");
+    }
+
+    if($(allInputs[i]).prop('id') > curreentHour ){
+        $(allInputs[i]).addClass("future");  
     
+    }
+
+    if($(allInputs[i]).prop('id') == curreentHour ){
+        $(allInputs[i]).addClass("present");
+    }
+
 }
 
-console.log(allInputs.length);
+inputValue = [];
+var buttonEl = $('button').on('click', saveContent);
 
+function saveContent(event){
 
+    const data = JSON.parse(localStorage.getItem('items'));
 
+    for(var i = 0; i < allInputs.length; i++){
 
+        if($(allInputs[i]).prop('id') == $(this).attr('id')){
+            console.log($(allInputs[i]).val());
 
+            if(data != null){
 
+                data.push($(allInputs[i]).val());
+                localStorage.setItem('items', JSON.stringify(data));
+                console.log("entro1");
+                console.log(data);
+            }
 
+            else{
+               
+                let a = $(allInputs[i]).val();
+                inputValue.push(a);
+                localStorage.setItem('items', JSON.stringify(inputValue));
+                console.log("entro2");
+            }
+           
+        }
 
-
-// var now = new Date();
-// var events = $('textarea');
-// function display_c(){
-//     var refresh=1000; 
-//     mytime=setTimeout('display_ct()',refresh);
-// }
- 
-// function display_ct() {
-//     var d = new Date();
-//     document.getElementById('currentDay').innerHTML = d;
-//     display_c();
-// }
-
-
-
-// for( var i = 0; i < $('textarea').lenght; i++){
+    }
     
-//     if(events.attr('aria-valuetext') < 12){
-//         $('textarea').addClass("past")
-//         $('textarea').prop("readonly", "true");
-//     }
-//     if(events.attr('aria-valuetext') > 12){
-//         $('textarea').addClass("future");
-//     }
-
-// }
-// console.log($('textarea').lenght);
-
-// if(now.getHours() == 13){
-//     $('#textarea13').addClass("present")
-// }
-
-// if(now.getHours() >! 14){
-//     $('#textarea14').addClass("future")  
-// }
-
-
+    event.preventDefault();
+}
